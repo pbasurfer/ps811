0a27b6ede601d0a1264a423820c4d065304ea36b
Joshua Firestone <jfirestone@wisc.edu>
2020-05-04 11:28:00 -0500

    partition plot

--- scripts/tree-regression.R
+++ scripts/tree-regression.R
@@ -30,8 +30,12 @@ plotcp(boston_tree)
 #0.035 is where the variance falls below the relevant level
 tree_prune <- prune(boston_tree, cp = 0.035)
 prp(tree_prune, extra = 1, box.col = "olivedrab3")
-
-
+#create partition to visualize splits
+#it only works with 2 predictor variables
+#so it only applies to tree_prune, which isn't the best model
+#but its pretty cool nonetheless!
+plot(boston$LSTAT, boston$RM, col = "olivedrab3", pch=20, xlab="LSTAT",ylab="RM")
+partition.tree(prune.tree(tree(target ~ RM + LSTAT, data = training), best = 5), ordvars=c("LSTAT","RM"), add=TRUE, cex = 1.15)
 
 tree_pred <- predict(tree_prune, newdata = test)
 RMSE_tree <- sqrt(mean((tree_pred - test$target)^2))
