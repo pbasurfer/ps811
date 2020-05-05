d790ca419d054e70cbef0d66f265863b216ca5a9
Joshua Firestone <jfirestone@wisc.edu>
2020-05-05 11:13:57 -0500

    use @knitr syntax for code chunks

--- scripts/tree-regression.R
+++ scripts/tree-regression.R
@@ -26,21 +26,25 @@ boston_tree <- rpart(target ~ CRIM + ZN + INDUS + CHAS + NOX + RM +
                        LSTAT, data = training, method = "anova")
 
 boston_tree$cptable
+## @knitr plotcp
 plotcp(boston_tree)
 #0.035 is where the variance falls below the relevant level
 tree_prune <- prune(boston_tree, cp = 0.035)
+## @knitr firsttree
 prp(tree_prune, extra = 1, box.col = "olivedrab3")
 #create partition to visualize splits
 #it only works with 2 predictor variables
 #so it only applies to tree_prune, which isn't the best model
 #but its pretty cool nonetheless!
+## @knitr partitiontree
 plot(boston$LSTAT, boston$RM, col = "olivedrab3", pch=20, xlab="LSTAT",ylab="RM")
 partition.tree(prune.tree(tree(target ~ RM + LSTAT, data = training), best = 5), ordvars=c("LSTAT","RM"), add=TRUE, cex = 1.15)
 
 #use minimum relative x-error as pruning criteria
 pdtree <- prune(boston_tree, cp=boston_tree$cptable[which.min(boston_tree$cptable[,"xerror"]),"CP"])
+## @knitr secondtree
 prp(pdtree, extra = 1, box.col = "olivedrab3")
-
+## @knitr stop
 tree_pred <- predict(pdtree, newdata = test)
 RMSE_tree <- sqrt(mean((tree_pred - test$target)^2))
 print(RMSE_tree)
@@ -57,8 +61,9 @@ rf_predict <- predict(rf_boston, test, type = "response")
 rf_rmse <- sqrt(mean((rf_predict - test$target)^2))
 print(rf_rmse)
 importance(rf_boston)
+## @knitr importanceplot
 varImpPlot(rf_boston, sort = TRUE)
-
+## @knitr stop
 boston_imp <- as.data.frame(importance(rf_boston))
 boston_imp$variables <- row.names(boston_imp)
 boston_imp[order(boston_imp$"%IncMSE", decreasing = "TRUE"), ]
